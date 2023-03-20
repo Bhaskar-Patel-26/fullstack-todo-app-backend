9933a0ba06c16351d7742fa58b0a59960a271705
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2023-03-20 18:45:56 +0530

    Add files via upload
    Backend done

---
+++ controllers/ToDoController.js
@@ -0,0 +1,29 @@
+const ToDoModel = require("../models/ToDoModels");
+
+module.exports.getToDo = async (req, res) => {
+  const toDo = await ToDoModel.find();
+  res.send(toDo);
+};
+
+module.exports.saveToDo = (req, res) => {
+  const { text } = req.body;
+  ToDoModel.create({ text }).then((data) => {
+    console.log("Added Successfully...");
+    console.log(data);
+    res.send(data);
+  });
+};
+
+module.exports.updateToDo = (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Updated Successfully..."))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Successfully..."))
+    .catch((err) => console.log(err));
+};
